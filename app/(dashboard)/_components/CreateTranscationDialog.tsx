c93515338f44fe32fb7b4f70074aa9a8a91df8b4
Hemant297 <hemant.khemka66@gmail.com>
2024-07-23 23:48:17 +0530

    2:08

--- app/(dashboard)/_components/CreateTranscationDialog.tsx
+++ app/(dashboard)/_components/CreateTranscationDialog.tsx
@@ -1,81 +1,121 @@
-"use client"
+"use client";
 import { DialogHeader } from "@/components/ui/dialog";
 import { TranscationType } from "@/lib/type";
 import { cn } from "@/lib/utils";
-import { Dialog, DialogTrigger, DialogContent, DialogTitle } from "@/components/ui/dialog";
-import { ReactNode} from "react"
-import { CreateTranscationSchema, CreateTranscationSchemaType} from "@/schema/transcations";
-import {zodResolver} from "@hookform/resolvers/zod"
-import { useForm} from "react-hook-form"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { ReactNode, useCallback } from "react";
+import {
+  CreateTranscationSchema,
+  CreateTranscationSchemaType,
+} from "@/schema/transcations";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-interface Props{
-    trigger: ReactNode;
-    type: TranscationType;
+import CategoryPicker from "./CategoryPicker";
+interface Props {
+  trigger: ReactNode;
+  type: TranscationType;
+}
+function CreateTranscationDialog({ trigger, type }: Props) {
+  const form = useForm<CreateTranscationSchemaType>({
+    resolver: zodResolver(CreateTranscationSchema),
+    defaultValues: {
+      type,
+      date: new Date(),
+    },
+  });
 
-}
-function CreateTranscationDialog({trigger, type}: Props){
-    const form = useForm<CreateTranscationSchemaType>({
-        resolver: zodResolver(CreateTranscationSchema),
-        defaultValues: {
-            type,
-            date: new Date()
-        }
-    })
-    return <Dialog>
-        <DialogTrigger asChild>
-            {trigger}
-        </DialogTrigger>
-        <DialogContent>
-            <DialogHeader>
-                <DialogTitle>Create a new <span className={cn("m-1", type === "income"? "text-emerald-500": "text-red-500")}>{type}</span> transcations </DialogTitle>
-            </DialogHeader>
-            <Form {...form}>
-                <form className="space-y-4">
-                    <FormField
-                    control={form.control}
-                    name="description"
-                    render={({ field })=>(
-                        <FormItem>
-                            <FormLabel>
-                                Description
-                            </FormLabel>
-                        <FormControl><Input defaultValue={""} {...field}/></FormControl>
-                        <FormDescription>Transcation Description (Optional) </FormDescription>
-                        </FormItem>
-                    )}
-                    />
-                    <FormField
-                    control={form.control}
-                    name="amount"
-                    render={({ field })=>(
-                        <FormItem>
-                            <FormLabel>
-                                Amount
-                            </FormLabel>
-                        <FormControl><Input defaultValue={0} type="number" {...field}/></FormControl>
-                        <FormDescription>Transcation Amount (Required) </FormDescription>
-                        </FormItem>
-                    )}
-                    />
-                    <div className="flex items-center justify-between gap-2">
-                        <FormField
-                    control={form.control}
-                    name="category"
-                    render={({ field })=>(
-                        <FormItem>
-                            <FormLabel>
-                                Category
-                            </FormLabel>
-                        <FormControl><Input defaultValue={""} {...field}/></FormControl>
-                        <FormDescription>Select a category for this transcation! </FormDescription>
-                        </FormItem>
-                    )}
-                    />
-                    </div>
-                </form>
-            </Form>
-        </DialogContent>
+  const handleChange = useCallback(
+    (value: string) => {
+      form.setValue("category", value);
+    },
+    [form]
+  );
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create a new
+            <span
+              className={cn(
+                "m-1",
+                type === "income" ? "text-emerald-500" : "text-red-500"
+              )}
+            >
+              {type}
+            </span>
+            transcation
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-4">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={""} {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transcation Description (Optional)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={0} type="number" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transcation Amount (Required){" "}
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-between gap-2">
+              <FormField
+                control={form.control}
+                name="category"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Category</FormLabel>
+                    <FormControl>
+                      <CategoryPicker type={type} onChange={handleChange} />
+                    </FormControl>
+                    <FormDescription>
+                      Select a category for this transcation!
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
     </Dialog>
+  );
 }
-export default CreateTranscationDialog
+export default CreateTranscationDialog;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -13,35 +13,45 @@ async function page() {
   }
   const userSettings = await prisma.userSettings.findUnique({
     where: {
-      userId: user.id
-    }
-  })
-  if(!userSettings){
-    redirect("/wizard")
+      userId: user.id,
+    },
+  });
+  if (!userSettings) {
+    redirect("/wizard");
   }
   return (
     <div className="h-full bg-background">
       <div className="border-b bg-card">
         <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-
-            <p className="text-3xl font-bold">Hello, {user.firstName}! 👋🏻</p>
-            <div className="flex items-center gap-3">
-
-              <CreateTranscationDialog trigger={
-          <Button  variant = {"outline"} className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 ">
-            New income 🤑
-          </Button>} type="income"/>
-          <CreateTranscationDialog trigger={
-           <Button variant = {"outline"} className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 ">
-            New Expense 😥
-          </Button>} type="expense" />
-  
+          <p className="text-3xl font-bold">Hello, {user.firstName}! 👋🏻</p>
+          <div className="flex items-center gap-3">
+            <CreateTranscationDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 "
+                >
+                  New income 🤑
+                </Button>
+              }
+              type="income"
+            />
+            <CreateTranscationDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 "
+                >
+                  New Expense 😥
+                </Button>
+              }
+              type="expense"
+            />
+          </div>
         </div>
-        </div>
-        
       </div>
     </div>
-  )
+  );
 }
 
 export default page;
